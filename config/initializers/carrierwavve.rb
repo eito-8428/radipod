08fac74076647f67910653160af632e48732494d
”ikebuchieito” <toei8428gmali.com>
2021-01-20 12:18:17 +0000

    waveradipod

--- config/initializers/carrierwavve.rb
+++ config/initializers/carrierwavve.rb
@@ -3,14 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = ENV['radipod']
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AKIARRS33554QNPSKKUU'],
-    aws_secret_access_key: ENV['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'],
-    region: ENV['ap-northeast-1'],
-    path_style: true
-  }
+      provider: 'AWS',
+      aws_access_key_id: 'AKIARRS33554QNPSKKUU',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory  = 'radipod'
+    config.cache_storage = :fog
+   
+  
 end
+
